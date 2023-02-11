cd7f5489995d4b43feec10574104dda8cd30859f
Sonarchik <Sonarchiko@gmail.com>
2023-02-11 20:00:16 +0200

    finish 3.0

--- src/main/java/DatabasePopulateService.java
+++ src/main/java/DatabasePopulateService.java
@@ -5,20 +5,87 @@ import java.sql.SQLException;
 public class DatabasePopulateService {
 
     public static void main(String[] args) {
-        String[] workersName = {"Djovany", "Axiles", "Leonid", "Kiril", "Piter",
-                "Anastasia", "Ludmila", "Andrey", "Kuzma", "Pavel"};
-        String[] workersBirthday = {"1979-01-15", "2003-06-11", "1989-02-28", "1998-08-09", "2001-05-11", "2005-05-21",
-                "1999-10-16", "1994-08-12", "1983-04-06", "1979-04-15"};
-        String[] workersLevel = {"Senior", "Trainee", "Trainee", "Trainee", "Junior", "Junior", "Middle", "Middle",
-                "Senior", "Senior"};
-        int[] workersSalary = {10000, 450, 600, 500, 1000, 1100, 2500, 3000, 6500, 10000};
-        String[] clientsName = {"Roman Borziy", "Jonny Deph", "Alex Prostoy", "Antonio Vdumchiviy", "Nikola Tesla",
-                "MOU", "NTUU"};
+        String[] workersName = {
+                "Djovany",
+                "Axiles",
+                "Leonid",
+                "Kiril",
+                "Piter",
+                "Anastasia",
+                "Ludmila",
+                "Andrey",
+                "Kuzma",
+                "Pavel"
+        };
+        String[] workersBirthday = {
+                "1979-01-15",
+                "2003-06-11",
+                "1989-02-28",
+                "1998-08-09",
+                "2001-05-11",
+                "2005-05-21",
+                "1999-10-16",
+                "1994-08-12",
+                "1983-04-06",
+                "1979-04-15"
+        };
+        String[] workersLevel = {
+                "Senior",
+                "Trainee",
+                "Trainee",
+                "Trainee",
+                "Junior",
+                "Junior",
+                "Middle",
+                "Middle",
+                "Senior",
+                "Senior"
+        };
+        int[] workersSalary = {
+                10000,
+                450,
+                600,
+                500,
+                1000,
+                1100,
+                2500,
+                3000,
+                6500,
+                10000
+        };
+        String[] clientsName = {
+                "Roman Borziy",
+                "Jonny Deph",
+                "Alex Prostoy",
+                "Antonio Vdumchiviy",
+                "Nikola Tesla",
+                "MOU", "NTUU"
+        };
         int[] clientsId = {7, 1, 1, 6, 5, 3, 2, 4, 3, 5};
-        String[] projectsStartDate = {"2022-02-17", "2022-08-23", "2022-09-05", "2022-11-21", "2022-12-03",
-                "2022-04-03", "2022-01-05", "2022-12-01", "2022-09-07", "2022-09-09"};
-        String[] projectsFinishDate = {"2022-07-01", "2028-06-03", "2024-05-04", "2029-04-05", "2027-07-06",
-                "2023-01-01", "2024-03-07", "2026-03-06", "2024-01-10", "2030-09-03"};
+        String[] projectsStartDate = {
+                "2022-02-17",
+                "2022-08-23",
+                "2022-09-05",
+                "2022-11-21",
+                "2022-12-03",
+                "2022-04-03",
+                "2022-01-05",
+                "2022-12-01",
+                "2022-09-07",
+                "2022-09-09"
+        };
+        String[] projectsFinishDate = {
+                "2022-07-01",
+                "2028-06-03",
+                "2024-05-04",
+                "2029-04-05",
+                "2027-07-06",
+                "2023-01-01",
+                "2024-03-07",
+                "2026-03-06",
+                "2024-01-10",
+                "2030-09-03"
+        };
         int[] projectsId = {1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 7, 7, 8, 8, 9, 9, 10, 10, 10, 10, 10};
         int[] workersId = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 3, 4, 5, 2, 4, 5, 1, 3, 1, 3, 9, 7, 8};
 
